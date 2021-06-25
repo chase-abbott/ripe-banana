c204ca7b02284d9986bb99d9623ed785d072cf02
thankrandomness <vijaybchakilam@gmail.com>
2021-06-25 07:01:39 -0400

    add GET all reviewers route

--- controllers/reviewers.js
+++ controllers/reviewers.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import Reviewer from '../lib/models/Reviewer';
 
-export default Router().post('/api/v1/reviewers', (req, res, next) => {
-  Reviewer.create(req.body)
-    .then((reviewer) => res.send(reviewer))
-    .catch(next);
-});
+export default Router()
+  .post('/api/v1/reviewers', (req, res, next) => {
+    Reviewer.create(req.body)
+      .then((reviewer) => res.send(reviewer))
+      .catch(next);
+  })
+  .get('/api/v1/reviewers', (req, res, next) => {
+    Reviewer.findAll()
+      .then((reviewers) => res.send(reviewers))
+      .catch(next);
+  });
