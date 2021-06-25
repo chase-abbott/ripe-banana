81d2e5a4a0ac7a64e030b47a66bc81f749d927ac
Chase <cabbott93@gmail.com>
2021-06-25 15:19:30 -0700

    started fixing reviewers

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -32,8 +32,8 @@ describe('Reviewer routes', () => {
   });
 
   // need to create reviews before testing to get a reviewer by id
-  //let review = {rating: 4, review: 'very good', film: }
-  it.skip('GET a reviewer by id from /api/v1/reviewers/:id', async () => {
+  it('GET a reviewer by id from /api/v1/reviewers/:id', async () => {
+    const review = { rating: 4, review: 'very good', film: 'Saving Private Ryan' };
     const response = await request.get(`/api/v1/reviewers/${reviewer.id}`);
     expect(response.status).toBe(200);
     expect(response.body).toStrictEqual([reviewer]);
