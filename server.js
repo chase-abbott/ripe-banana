22941f4916e72286b015fa3c713e2e65549682f8
thankrandomness <vijaybchakilam@gmail.com>
2021-06-29 13:54:52 -0400

    deploy to heroku - wip

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-console */
 import app from './lib/app.js';
 import pool from './lib/utils/pool.js';
 
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
 
